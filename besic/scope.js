c50a5bcc462ebb3ff7bc1ab702abb8b79bf5b1e4
namulnobel <nobelnaimulislam@gmail.com>
2021-01-21 17:00:08 +0600

    javascript besic

---
+++ besic/scope.js
@@ -0,0 +1,16 @@
+// var varOne='varOne'
+
+// if(true){
+//    console.log(varOne)
+// }
+let name= 'nobel'
+if (true){
+    if(true){
+        let name="mike"
+        console.log(name)
+
+    }
+}
+if (true){
+    console.log(name)
+}
