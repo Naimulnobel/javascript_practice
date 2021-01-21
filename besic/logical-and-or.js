516037674c8527666a897c389f7141bd69e4ed0b
namulnobel <nobelnaimulislam@gmail.com>
2021-01-21 13:57:37 +0600

    javascript besic

---
+++ besic/logical-and-or.js
@@ -0,0 +1,11 @@
+let guestOneVegan=true
+let guestTwoVegan=false
+
+if (guestOneVegan===true && guestTwoVegan==true){
+    console.log('Offer vegan food')
+}else if(guestOneVegan===true ||guestTwoVegan==true){
+    console.log('Offer some vegan food')
+
+}else{
+    console.log('pickup any menu you like')
+}
