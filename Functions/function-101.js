0ebff9388934f751cc53c7b71b4eb55aefa58bd7
namulnobel <nobelnaimulislam@gmail.com>
2021-01-21 17:43:18 +0600

    functions practice

---
+++ Functions/function-101.js
@@ -0,0 +1,19 @@
+// let greetUser= function(){
+//     console.log('Welcome User')
+// }
+// greetUser()
+
+// let sum=function(number1,number2){
+//     return number1+ number2;
+// }
+// console.log(sum(4,3))
+let temp
+
+let celcius=function(temp){
+    console.log((temp-32)*(5/9))
+}
+let kelvin=function(temp){
+    console.log((temp+459.67)*(5/9))
+}
+celcius(32)
+kelvin(32)
---
+++ Functions/undifined-null.js
@@ -0,0 +1,6 @@
+let number
+
+let square=function(number){
+    console.log(number*number)
+}
+square(3)
